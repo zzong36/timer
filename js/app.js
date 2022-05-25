bfd3ffcb335dba390565ac18f79487a48a575dec
EomJaeCheol <abctmxk@naver.com>
2022-05-25 14:32:30 +0900

    장학정보

--- js/app.js
+++ js/app.js
@@ -6,3 +6,25 @@ toggleBtn.addEventListener("click", () => {
     menu.classList.toggle('active');
     submenu.classList.toggle('active');
 });
+
+// 검색결과 dom 에 뿌려주기
+
+const btn = document.querySelector(".btn");
+const result = document.querySelector(".detail");
+const vaild = document.getElementById("Search");
+
+
+btn.addEventListener("click", () =>{
+    if(vaild.value == ""){
+        alert("검색어를 입력해주세요!");
+        vaild.focus();
+    }else{
+        result.innerHTML += "<h1 onclick='popup()'>장학금 정보</h1>";
+    }
+});
+
+function popup() {
+    window.open("Scholarshipinfo.html", "장학금 정보", "width=800, height=800, left=100, top=50");
+}
+
+    
